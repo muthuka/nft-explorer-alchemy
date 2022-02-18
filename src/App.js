367668a3b154ab123c7a8ac6fe1524450a5a1341
Muthu <muthu.kumar@me.com>
2022-02-18 12:19:23 -0500

    Added tutorial code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider, defaultChains } from "wagmi"
+import { InjectedConnector } from "wagmi/connectors/injected"
+import Nav from './components/nav';
 
 function App() {
+  // initialize wagmi library connectors for Metamask and Walletconnect
+  const connectors = () => {
+    return [
+      new InjectedConnector({ defaultChains }),
+    ]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider autoConnect connectors={connectors}>
+      <Nav>
+      </Nav>
+    </Provider>
   );
 }
 
